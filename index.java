9d100a839bbeff49fe143b2cb012e605799ecd9e
jakza <142193881+JakaZxx@users.noreply.github.com>
2023-09-12 11:59:53 +0700

    Add files via upload

---
+++ index.java
@@ -0,0 +1,7 @@
+
+
+System.out.println("Halo, dunia!"); 
+
+
+
+// Program sederhana untuk menambahkan dua angka int angka1 = 5; int angka2 = 3; int hasil = angka1 + angka2; System.out.println("Hasil penjumlahan: " + hasil);
